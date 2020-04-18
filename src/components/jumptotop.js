f6a15b1ded2786433f341147296c4afae6b8c759
TwanMulder <twan.mulder@gmail.com>
2020-04-18 22:06:27 +0200

    Fixed svg properties in scrolltotop component

--- src/components/jumptotop.js
+++ src/components/jumptotop.js
@@ -74,10 +74,10 @@ export default class JumpToTop extends React.Component {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-arrow-up"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="feather feather-arrow-up"
         >
           <line x1="12" y1="19" x2="12" y2="5"></line>
           <polyline points="5 12 12 5 19 12"></polyline>
