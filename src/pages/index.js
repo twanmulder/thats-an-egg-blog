b071cd7cab7162fc26e8e4f9272e49a355d63a18
TwanMulder <twan.mulder@gmail.com>
2020-04-23 22:30:48 +0200

    Optimized index page styling and rewrote it to functional component

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,91 +27,92 @@ const StyledHeader = styled.h1`
     align-items: center;
 
     .gatsby-image-wrapper {
-      margin: 0 ${rhythm(3 / 4)} 0 0 !important;
+      margin: 0 ${rhythm(3 / 4)} 0 0;
     }
   }
 `
 
-export default class IndexPage extends React.Component {
-  render() {
-    const oneDay = 24 * 60 * 60 * 1000 // hours*minutes*seconds*milliseconds
-    const firstDate = new Date(2019, 3, 1)
-    const secondDate = new Date()
-    const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay))
+const StyledAvatar = styled(Image)`
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: ${rhythm(3 / 4)};
+  border-radius: 100%;
 
-    return (
-      <Layout>
-        <SEO
-          title="Home"
-          keywords={[`developer`, `portfolio`, `javascript`, `react`, `blog`]}
-        />
-        <StyledHeader>
-          <StaticQuery
-            query={avatarQuery}
-            render={data => {
-              return (
-                <Image
-                  fixed={data.avatar.childImageSharp.fixed}
-                  alt="Twan Mulder"
-                  style={{
-                    marginRight: "auto",
-                    marginLeft: "auto",
-                    marginBottom: rhythm(3 / 4),
-                    minWidth: 75,
-                    borderRadius: `100%`,
-                    display: `block`,
-                  }}
-                  imgStyle={{
-                    borderRadius: `50%`,
-                  }}
-                />
-              )
-            }}
-          />
-          <span>Hey there, I'm Twan </span>
-          <span role="img" aria-label="peace emoji">
-            ✌️
-          </span>
-        </StyledHeader>
-        <p>
-          <strong>
-            I'm a Creative Developer and Technical CRO Consultant from the
-            Netherlands.
-          </strong>
-          <br />
-          Currently, I've been employed at{" "}
-          <a
-            href="https://stormdigital.nl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Storm Digital
-          </a>{" "}
-          for {diffDays} days.
-        </p>
-        <p>
-          These days, web-development can be a very daunting task to learn for
-          the developer who's just starting out. With things like JavaScript
-          frameworks, server-side rendering, and REST/CRUD API's, it doesn't get
-          a lot simpler either.
-        </p>
-        <p>
-          <strong>However, it doesn't have to be this way!</strong> I love
-          writing about making your (developer) life easier. From basic HTML &
-          CSS, to more complicated subjects regarding web-development, I write
-          about all of them.
-        </p>
-        <p>
-          You might know the expression "That's a piece of cake". In Dutch, we
-          have a saying that goes likewise. Roughly translated back to English,
-          it says <strong>"That's an Egg"</strong>. With my writing, I want to
-          make you feel confident in your ability as a developer and think;
-          "That's an Egg!".
-        </p>
-        <Link to="/blog/">
-          <Button>That's an Egg Blog</Button>
-        </Link>
-      </Layout>
-    )
+  img {
+    border-radius: 50%;
   }
+`
+
+function IndexPage() {
+  const oneDay = 24 * 60 * 60 * 1000 // hours*minutes*seconds*milliseconds
+  const firstDate = new Date(2019, 3, 1)
+  const secondDate = new Date()
+  const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay))
+
+  return (
+    <Layout>
+      <SEO
+        title="Home"
+        keywords={[`developer`, `portfolio`, `javascript`, `react`, `blog`]}
+      />
+      <StyledHeader>
+        <StaticQuery
+          query={avatarQuery}
+          render={data => {
+            return (
+              <StyledAvatar
+                fixed={data.avatar.childImageSharp.fixed}
+                alt="Twan Mulder"
+                style={{ display: `block` }}
+              />
+            )
+          }}
+        />
+        <span>Hey there, I'm Twan </span>
+        <span role="img" aria-label="peace emoji">
+          ✌️
+        </span>
+      </StyledHeader>
+      <p>
+        <strong>
+          I'm a Creative Developer and Technical CRO Consultant from the
+          Netherlands.
+        </strong>
+        <br />
+        Currently, I've been employed at{" "}
+        <a
+          href="https://stormdigital.nl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Storm Digital
+        </a>{" "}
+        for {diffDays} days.
+      </p>
+      <p>
+        These days, web-development can be a very daunting task to learn for the
+        developer who's just starting out. With things like JavaScript
+        frameworks, server-side rendering, and REST/CRUD API's, it doesn't get a
+        lot simpler either.
+      </p>
+      <p>
+        <strong>However, it doesn't have to be this way!</strong> I love writing
+        about making your (developer) life easier. From basic HTML & CSS, to
+        more complicated subjects regarding web-development, I write about all
+        of them.
+      </p>
+      <p>
+        You might know the expression "That's a piece of cake". In Dutch, we
+        have a saying that goes likewise. Roughly translated back to English, it
+        says <strong>"That's an Egg"</strong>. With my writing, I want to make
+        you feel confident in your ability as a developer and think; "That's an
+        Egg!".
+      </p>
+      <Link to="/blog/">
+        <Button>That's an Egg Blog</Button>
+      </Link>
+    </Layout>
+  )
 }
+
+export default IndexPage
