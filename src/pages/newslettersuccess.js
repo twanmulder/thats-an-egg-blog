2e07dadd9de8eb97b15ae2ddcdf1b685c77b5265
TwanMulder <twan.mulder@gmail.com>
2020-04-29 21:26:47 +0200

    Created newsletter succes page

---
+++ src/pages/newslettersuccess.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Button from "../components/button"
+
+function NewsletterSuccess() {
+  return (
+    <Layout hideFooter="true">
+      <h1>You're in!</h1>
+      <p>Thank you for subscribing to "That's an Egg"s updates.</p>
+
+      <Link to="/blog">
+        <Button>Read some articles</Button>
+      </Link>
+    </Layout>
+  )
+}
+
+export default NewsletterSuccess
