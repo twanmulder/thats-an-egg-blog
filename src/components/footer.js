13276251c0db3838d82dfe13fee52dd5f1c97626
TwanMulder <twan.mulder@gmail.com>
2020-04-29 22:40:00 +0200

    Updated mobile footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,35 +6,45 @@ import { rhythm } from "../utils/typography"
 
 const StyledFooter = styled.footer`
   margin-top: ${rhythm(1.5)};
+  text-align: center;
+
+  @media (min-width: 768px) {
+    text-align: left;
+  }
 `
 
-const RSSLink = styled(Link)`
-  float: right;
+const RSSLink = styled.div`
+  margin-top: ${rhythm(0.75)};
+
+  @media (min-width: 768px) {
+    margin-top: 0;
+    float: right;
+  }
 `
 
 export default function Footer() {
   return (
     <StyledFooter>
-      <p>
-        <a
-          href="https://twitter.com/toktoktwan"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
-        &nbsp;•&nbsp;
-        <a
-          href="https://github.com/twanmulder"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-        &nbsp;•&nbsp;
-        <Link to="/privacypolicy">Privacy Policy</Link>
-        <RSSLink to="/rss.xml">RSS</RSSLink>
-      </p>
+      <a
+        href="https://twitter.com/toktoktwan"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+      &nbsp;•&nbsp;
+      <a
+        href="https://github.com/twanmulder"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+      &nbsp;•&nbsp;
+      <Link to="/privacypolicy">Privacy Policy</Link>
+      <RSSLink>
+        <Link to="/rss.xml">RSS</Link>
+      </RSSLink>
     </StyledFooter>
   )
 }
