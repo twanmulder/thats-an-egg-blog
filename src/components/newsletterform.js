63ca43c26958f149717a9b78db8d84aae4774e36
TwanMulder <twan.mulder@gmail.com>
2020-05-03 17:36:15 +0200

    Fixed JSX and linter warning in newsletter form

--- src/components/newsletterform.js
+++ src/components/newsletterform.js
@@ -7,7 +7,7 @@ function NewsletterForm() {
   return (
     <form
       action="https://app.convertkit.com/forms/1356313/subscriptions"
-      class="seva-form formkit-form"
+      className="seva-form formkit-form"
       method="post"
       data-sv-form="1356313"
       data-uid="812b8a9345"
@@ -22,20 +22,20 @@ function NewsletterForm() {
         style={{ display: `none` }}
       ></input>
       <div data-style="full">
-        <div data-element="column" class="formkit-background"></div>
-        <div data-element="column" class="formkit-column">
-          <div class="formkit-header" data-element="header">
+        <div data-element="column" className="formkit-background"></div>
+        <div data-element="column" className="formkit-column">
+          <div className="formkit-header" data-element="header">
             <h1>Receive updates from me!</h1>
           </div>
           <ul
-            class="formkit-alert formkit-alert-error"
+            className="formkit-alert formkit-alert-error"
             data-element="errors"
             data-group="alert"
           ></ul>
-          <div data-element="fields" class="seva-fields formkit-fields">
-            <div class="formkit-field">
+          <div data-element="fields" className="seva-fields formkit-fields">
+            <div className="formkit-field">
               <input
-                class="formkit-input"
+                className="formkit-input"
                 aria-label="Your first name"
                 name="fields[first_name]"
                 placeholder="Your first name"
@@ -43,18 +43,20 @@ function NewsletterForm() {
                 required
               />
             </div>
-            <div class="formkit-field">
+            <div className="formkit-field">
               <input
-                class="formkit-input"
+                className="formkit-input"
                 name="email_address"
                 placeholder="Your email address"
-                required=""
                 type="email"
                 required
               />
             </div>
-            <button data-element="submit" class="formkit-submit formkit-submit">
-              <div class="formkit-spinner">
+            <button
+              data-element="submit"
+              className="formkit-submit formkit-submit"
+            >
+              <div className="formkit-spinner">
                 <div></div>
                 <div></div>
                 <div></div>
@@ -62,12 +64,12 @@ function NewsletterForm() {
               <span>Subscribe</span>
             </button>
           </div>
-          <div class="formkit-disclaimer" data-element="disclaimer">
+          <div className="formkit-disclaimer" data-element="disclaimer">
             <p>I respect your privacy. Unsubscribe at anytime. No spam.</p>
           </div>
           <a
             href="https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form"
-            class="formkit-powered-by"
+            className="formkit-powered-by"
             data-element="powered-by"
             target="_blank"
             rel="noopener noreferrer"
