f2431d7b1b901817d9ea611ce997f3b30cf4ab45
TwanMulder <twan.mulder@gmail.com>
2020-10-30 10:05:02 +0100

    Split up hero as seperate component

---
+++ src/components/hero.js
@@ -0,0 +1,148 @@
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+import Image from "gatsby-image"
+import styled from "styled-components"
+
+const HeroWrapper = styled.section`
+  position: relative;
+  background: #fec150;
+  margin-bottom: -7rem;
+
+  > div {
+    width: 100%;
+    margin: 0 auto;
+    max-width: 800px;
+    padding: 40px 40px 0 40px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 767px) {
+    margin-bottom: -5.25rem;
+
+    > div {
+      padding: 20px 20px;
+    }
+  }
+
+  @media (max-width: 1023px) {
+    > div {
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+`
+
+const HeroSmallImageWrapper = styled.div`
+  display: none;
+  visibility: hidden;
+
+  @media (max-width: 1023px) {
+    display: block;
+    visibility: visible;
+    width: 160px;
+    height: 160px;
+    margin-bottom: 25px;
+
+    > div {
+      background: rgba(255, 255, 255, 0.5);
+    }
+  }
+`
+
+const HeroTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    color: #1a1103;
+    margin: 0;
+  }
+
+  @media (max-width: 1023px) {
+    h1 {
+      padding-bottom: 60px;
+    }
+  }
+
+  @media (max-width: 767px) {
+    h1 {
+      max-width: 290px;
+      font-size: 25px;
+      padding-bottom: 10px;
+    }
+  }
+`
+
+const HeroImageWrapper = styled.div`
+  margin-right: -160px;
+  width: 420px;
+  height: 420px;
+  display: flex;
+
+  img {
+    max-width: 100%;
+    margin-bottom: 0;
+  }
+
+  @media (max-width: 1023px) {
+    display: none;
+    visibility: hidden;
+  }
+`
+
+export default function Hero() {
+  return (
+    <StaticQuery
+      query={heroQuery}
+      render={data => {
+        return (
+          <HeroWrapper>
+            <div>
+              <HeroSmallImageWrapper>
+                <Image
+                  fixed={data.avatarSmall.childImageSharp.fixed}
+                  className="hero-small-image"
+                  style={{
+                    borderRadius: `100%`,
+                  }}
+                  imgStyle={{
+                    borderRadius: `50%`,
+                  }}
+                ></Image>
+              </HeroSmallImageWrapper>
+
+              <HeroTitleWrapper>
+                <h1>Hi, I'm Twan Mulder. I help make your developer life easier.</h1>
+              </HeroTitleWrapper>
+              <HeroImageWrapper>
+                <Image fixed={data.avatarLarge.childImageSharp.fixed}></Image>
+              </HeroImageWrapper>
+            </div>
+          </HeroWrapper>
+        )
+      }}
+    />
+  )
+}
+
+export const heroQuery = graphql`
+  query HeroQuery {
+    avatarLarge: file(absolutePath: { regex: "/twan_transparent.png/" }) {
+      childImageSharp {
+        fixed(width: 420, height: 420, quality: 100) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    avatarSmall: file(absolutePath: { regex: "/twan_transparent.png/" }) {
+      childImageSharp {
+        fixed(width: 160, height: 160, quality: 100) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,103 +1,13 @@
 import React, { Fragment } from "react"
 import { Link, graphql, StaticQuery } from "gatsby"
-import Image from "gatsby-image"
 import styled from "styled-components"
 
 import { rhythm, boldWeight } from "../utils/typography"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Hero from "../components/hero"
 import ChevronRight from "../components/chevronright"
-
-const Hero = styled.section`
-  position: relative;
-  background: #fec150;
-  margin-bottom: -7rem;
-
-  > div {
-    width: 100%;
-    margin: 0 auto;
-    max-width: 800px;
-    padding: 40px 40px 0 40px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  @media (max-width: 767px) {
-    margin-bottom: -5.25rem;
-
-    > div {
-      padding: 20px 20px;
-    }
-  }
-
-  @media (max-width: 1023px) {
-    > div {
-      flex-direction: column;
-      align-items: center;
-    }
-  }
-`
-
-const HeroSmallImageWrapper = styled.div`
-  display: none;
-  visibility: hidden;
-
-  @media (max-width: 1023px) {
-    display: block;
-    visibility: visible;
-    width: 160px;
-    height: 160px;
-    margin-bottom: 25px;
-
-    > div {
-      background: rgba(255, 255, 255, 0.5);
-    }
-  }
-`
-
-const HeroTitleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  h1 {
-    color: #1a1103;
-    margin: 0;
-  }
-
-  @media (max-width: 1023px) {
-    h1 {
-      padding-bottom: 60px;
-    }
-  }
-
-  @media (max-width: 767px) {
-    h1 {
-      max-width: 290px;
-      font-size: 25px;
-      padding-bottom: 10px;
-    }
-  }
-`
-
-const HeroImageWrapper = styled.div`
-  margin-right: -160px;
-  width: 420px;
-  height: 420px;
-  display: flex;
-
-  img {
-    max-width: 100%;
-    margin-bottom: 0;
-  }
-
-  @media (max-width: 1023px) {
-    display: none;
-    visibility: hidden;
-  }
-`
 
 const StyledPostWrapper = styled.section`
   position: relative;
@@ -199,31 +109,7 @@ function IndexPage(props) {
           <Fragment>
             <Layout wrapperFormat="full" navStyle="hero">
               <SEO title="Articles making your developer life easier" keywords={[`developer`, `portfolio`, `javascript`, `react`, `blog`]} />
-
-              <Hero>
-                <div>
-                  <HeroSmallImageWrapper>
-                    <Image
-                      fixed={data.avatarSmall.childImageSharp.fixed}
-                      className="hero-small-image"
-                      style={{
-                        borderRadius: `100%`,
-                      }}
-                      imgStyle={{
-                        borderRadius: `50%`,
-                      }}
-                    ></Image>
-                  </HeroSmallImageWrapper>
-
-                  <HeroTitleWrapper>
-                    <h1>Hi, I'm Twan Mulder. I help make your developer life easier.</h1>
-                  </HeroTitleWrapper>
-                  <HeroImageWrapper>
-                    <Image fixed={data.avatarLarge.childImageSharp.fixed}></Image>
-                  </HeroImageWrapper>
-                </div>
-              </Hero>
-
+              <Hero />
               <StyledPostWrapper>
                 <h4>Most recent article</h4>
                 {posts.map(({ node }) => {
@@ -257,20 +143,6 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query PageQuery {
-    avatarLarge: file(absolutePath: { regex: "/twan_transparent.png/" }) {
-      childImageSharp {
-        fixed(width: 420, height: 420, quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-    avatarSmall: file(absolutePath: { regex: "/twan_transparent.png/" }) {
-      childImageSharp {
-        fixed(width: 160, height: 160, quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
     site {
       siteMetadata {
         title
