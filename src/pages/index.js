7775b3d3522c2c35c49425dc594941bb7ea1ee31
TwanMulder <twan.mulder@gmail.com>
2020-11-02 00:34:19 +0100

    Add links to twitter and newsletter on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import styled from "styled-components"
 
 import { rhythm, boldWeight } from "../utils/typography"
 
+import Twitter from "../../static/assets/icons/twitter.js"
+import Send from "../../static/assets/icons/send.js"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
@@ -99,6 +102,58 @@ const StyledLink = styled(Link)`
   }
 `
 
+const IndexLinks = styled.section`
+  margin: ${rhythm(3)} auto;
+  padding: 0 20px;
+  max-width: 800px;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 370px);
+  gap: 20px;
+
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+  }
+
+  a {
+    display: flex;
+    flex-direction: column;
+    border-radius: 0.5rem;
+    padding: 20px;
+    transition: all 0.3s ease-in-out 0s;
+
+    :hover {
+      transform: scale(1.03);
+      opacity: 0.9;
+      box-shadow: rgba(0, 0, 0, 0.15) 0px 10px 30px -5px;
+    }
+  }
+
+  p {
+    flex: 1 1;
+    margin: 0;
+    font-size: 1.25rem;
+  }
+
+  .twitter-link {
+    background: #1da1f2;
+    color: #fff;
+  }
+
+  .newsletter-link {
+    background: #fec150;
+    color: #1a1103;
+  }
+
+  svg {
+    flex: 1 1;
+    margin: 2rem 0 0 auto;
+    width: 32px;
+    height: 32px;
+    opacity: 0.5;
+  }
+`
+
 function IndexPage(props) {
   const { data } = props
   const posts = data.allMdx.edges
@@ -133,6 +188,16 @@ function IndexPage(props) {
                   )
                 })}
               </StyledPostWrapper>
+              <IndexLinks>
+                <a href="https://twitter.com/toktoktwan" target="_blank" rel="noopener noreferrer" className="twitter-link">
+                  <p>Follow me on Twitter</p>
+                  <Twitter />
+                </a>
+                <Link to="/newsletter" className="newsletter-link">
+                  <p>Join the Newsletter</p>
+                  <Send />
+                </Link>
+              </IndexLinks>
             </Layout>
           </Fragment>
         )
