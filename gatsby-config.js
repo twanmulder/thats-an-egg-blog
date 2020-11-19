b3a9daf4b70e3b84e2f6170e3ad1ad6da1b4cfae
TwanMulder <twan.mulder@gmail.com>
2020-11-19 21:13:37 +0100

    add preact for performance boost

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-feed-mdx`,
+    `gatsby-plugin-preact`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
