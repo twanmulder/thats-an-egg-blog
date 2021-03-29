3f01fa2e77872e9cc785fbb7927340cac5b07c64
twanmulder <twan.mulder@gmail.com>
2021-03-29 13:02:02 +0200

    pls fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,7 +39,7 @@ exports.createPages = ({ graphql, actions }) => {
       const next = index === 0 ? null : posts[index - 1].node
 
       createPage({
-        path: `blog${post.node.fields.slug}`,
+        path: `blog/${post.node.frontmatter.path}`,
         component: blogPost,
         context: {
           slug: post.node.fields.slug,
